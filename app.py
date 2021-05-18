b921ebd6dfeb9a4d30115d427d47f547232c60e9
ritikdutta <ritikduttagd@gmail.com>
2021-05-18 12:13:58 +0530

    aded else condition fixed

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def webhook():
 		speech = "value of {0} is {1}".format(b, r_crypto[b]['last'])
 
 	else:
-		speech = = "empty"
+		speech = "empty"
 
 	reply = {"candidates": [{"first_simple": {"variants": [{"speech": "Enter "}]}}]}
 	# print(b)
